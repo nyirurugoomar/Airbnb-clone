07e803cbb3ffbfdb220a9a7b2673849caf7fadaa
nyirurugo omar <nyirurugoomar@gmail.com>
2023-10-21 14:49:21 +0200

    Navbar and Footer

---
+++ app/page.js
@@ -0,0 +1,29 @@
+
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+  <>
+  
+  {/* Navbar */}
+  <Navbar/>
+
+
+  {/* Categories */}
+
+
+  {/* Cards */}
+
+
+  {/* Footer */}
+  <Footer/>
+
+  
+  </>
+    
+  )
+}
+ 
